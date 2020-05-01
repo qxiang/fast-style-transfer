74bfe7860144196f320f3798652bea48c368b7a1
Qian Xiang <qxiang@Qians-MacBook-Pro.local>
2020-04-30 22:03:30 -0400

    update train model

--- train.py
+++ train.py
@@ -9,9 +9,9 @@ import numpy as np
 import tensorflow as tf
 import PIL
 import hyperparameters as hp
-import run
+from run import preprocess, postprocess
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
-from tensorflow.keras.layers import Conv2D, Conv2DTranspose, BatchNormalization, Activation, Add, Concatenate
+from tensorflow.keras.layers import Conv2D, Conv2DTranspose, BatchNormalization, Activation
 
 model = None
 target_image = None
@@ -22,113 +22,137 @@ target_path = "./trivial_result/output_content.jpg"
 
 class ResBlock(tf.keras.Model):
 	def __init__(self):
-		super(ResBlock, self).__init__()
+		super.__init__()
 		self.model = [
-			Conv2D(128, 3, 1, padding="same", activation="relu", name="block2_conv1"),
+			Conv2D(128, 3, 1, padding="same", name="block2_conv1"),
 			BatchNormalization(),
-			Conv2D(128, 3, 1, padding="same", activation=None, name="block2_conv2"),
+			Activation('relu'),
+			Conv2D(128, 3, 1, padding="same", name="block2_conv2"),
 			BatchNormalization()
 		]
-		self.concat = Concatenate()
 		self.activation = Activation('relu')
 
 	def call(self, img):
 		residual = img
 		for layer in self.model:
 			img = layer(img)
-		img = self.concat([img, residual])
-		img = self.activation(img)
-		return img
+		return self.activation(img + residual)
 
 class TransformNet(tf.keras.Model):
 	def __init__(self):
-		super(TransformNet, self).__init__()
+		super.__init__()
 		self.conv_step = [
-			Conv2D(32, 3, 1, padding="same", activation="relu", name="block1_conv1"),
-			Conv2D(64, 3, 1, padding="same", activation="relu", name="block1_conv2"),
-			Conv2D(128, 3, 1, padding="same", activation="relu", name="block1_conv3")
+			Conv2D(32, 3, 2, padding="same", name="block1_conv1"),
+			BatchNormalization(),
+			Activation('relu'),
+			Conv2D(64, 3, 2, padding="same", name="block1_conv2"),
+			BatchNormalization(),
+			Activation('relu'),
+			Conv2D(128, 3, 2, padding="same", name="block1_conv3"),
+			BatchNormalization(),
+			Activation('relu')
 		]
-		self.res_step = [ResBlock(), ResBlock(), ResBlock()]
+		self.res_step = [ResBlock(), ResBlock(), ResBlock(), ResBlock(), ResBlock()]
 		self.deconv_step = [
-			Conv2D(64, 3, 1, padding="same", activation="relu", name="block3_conv1"),
-			Conv2D(32, 3, 1, padding="same", activation="relu", name="block3_conv2"),
-			#Conv2D(64, 3, 1, padding="same", activation="relu", name="block3_deconv1"),
-			#Conv2D(32, 3, 1, padding="same", activation="relu", name="block3_deconv2"),
-			Conv2D(16, 1, 1, padding="same", activation="relu", name="block3_conv3"),
-			Conv2D(3, 1, 1, padding="same", activation=None, name="block3_conv4")
+			Conv2DTranspose(64, 3, 2, padding="same", name="block3_conv1"),
+			BatchNormalization(),
+			Activation('relu'),
+			Conv2DTranspose(32, 3, 2, padding="same", name="block3_conv2"),
+			BatchNormalization(),
+			Activation('relu'),
+			Conv2DTranspose(3, 1, 2, padding="same", name="block3_conv3"),
+			BatchNormalization(),
+			Activation('tanh')
 		]
+		self.model = self.conv_step + self.res_step + self.deconv_step
 	
 	def call(self, img):
-		for layer in self.conv_step:
-			img = layer(img)
-		for layer in self.res_step:
-			img = layer(img)
-		for layer in self.deconv_step:
+		for layer in self.model:
 			img = layer(img)
 		return img
 
-class StyleTransferModel(tf.keras.Model):
+class VGGModel(tf.keras.Model):
 	def __init__(self):
-		super(StyleTransferModel, self).__init__()
-		self.vgg19 = run.VGGModel()
+		super().__init__()
+		content_layers_name = ['block1_conv1', 'block1_conv2', 'block2_conv1', 'block2_conv2', 'block3_conv1', 'block3_conv2']
+		style_layers_name = ['block1_conv1', 'block2_conv1', 'block3_conv1', 'block4_conv1', 'block5_conv1']
+		# Architecture
+		self.vgg19 = tf.keras.applications.VGG19(include_top=False, weights='imagenet')
+		self.vgg19.trainable = False
+		self.vgg19 = tf.keras.Model(
+			inputs=[self.vgg19.input],
+			outputs=[self.vgg19.get_layer(name).output for name in (content_layers_name + style_layers_name)])
+		self.vgg19.trainable = False
+	
+	def call(self, img):
+		""" Passes the image through the network. """
+		result = self.vgg19(img)
+		content, style = result[0:len(content_layers_name)], result[len(content_layers_name):len(result)]
+		return np.array(content), np.array(style)
+
+
+class StyleTransferModel(tf.keras.Model):
+	def __init__(self, image, style):
+		super().__init__()
+		self.vgg19 = VGGModel()
 		self.vgg19.trainable = False
 		self.transform = TransformNet()
 		self.transform.trainable = True
+		self.image = image
+		self.content_target, self.style_target = self.call(image)[0], self.call(style)[1]
 	
-	def call(self, img):
-		img = self.transform(img)
-		feature_map = self.vgg19(img)
-		return feature_map, img
+	def call(self):
+		return self.vgg19(self.transform(self.image))
 
-'''
-For convenience: I guess we need to change it for multi-style
-'''
-@tf.function
-def get_loss(feature_map, img):
-	#5e+6
-	loss = tf.reduce_sum((target_image - img) ** 2)
-	#loss += run.get_loss(feature_map)
-	return loss
+	def get_transfer_img(self):
+		return self.transform(self.image)
+
+	def content_loss(self, content):
+		# content_loss_val = tf.add_n([tf.reduce_sum((content[i] - content_target[i]) ** 2) / 2. for i in range(len(content))]) / len(content)
+		return np.mean(np.square(content - self.content_target)) 
+
+	def style_loss(self, style):
+		# factor = [tf.cast((2 * tf.shape(style[i])[1] * tf.shape(style[i])[2]), tf.float32) ** 2 for i in range(len(style))]
+		# style_loss_val = tf.add_n([tf.reduce_sum((style[i] - style_target[i]) ** 2) / factor[i] for i in range(len(style))]) / len(style)
+		return np.mean(np.square(gram(style) - gram(self.style_target)))
+
+	def get_loss(self):
+		content, style = self.call(img)
+		loss = hp.alpha * content_loss(content) + hp.beta * style_loss(content)
+		return loss
+
+def gram(style):
+	style = np.tensordot(style, style, axes=(0, 1)),
+	return style
 
 @tf.function
-def train_step(image):
-	global model
+def train_step(model):
 	with tf.GradientTape() as g:
-		feature_map, img = model(image)
-		loss = get_loss(feature_map, img)
+		loss = get_loss()
+
 	train_vars = model.trainable_variables
 	grad = g.gradient(loss, train_vars)
 	optimizer.apply_gradients(zip(grad, train_vars))
+	img = model.transfer_img()
 	return loss, img
 
 def main():
-	global model, target_image
-	model = StyleTransferModel()
-	# Generate data for content
-	#data_gen = tf.keras.preprocessing.image.ImageDataGenerator(preprocessing_function=tf.keras.applications.vgg19.preprocess_input)
-	#data_gen = data_gen.flow_from_directory("./train_data", target_size=(hp.img_size, hp.img_size), class_mode=None, batch_size=1)
-	# TODO: Train the net
-	# Import the images
-	run.content_image, run.style_image = run.preprocess(run.content_image_path), run.preprocess(run.style_image_path)
-	target_image = run.preprocess(target_path)
-	# Create model
-	run.model = run.VGGModel()
-	# Extract content and style
-	run.content_target, _ = run.model(run.content_image)
-	_, run.style_target = run.model(run.style_image)
-	run.style_target = [run.gram(style) for style in run.style_target]
+	content_image_path = './trivial_data/buildings.jpg'
+	style_image_path = './trivial_data/starry-sky.jpg'
+	content_image, style_image = preprocess(content_image_path), preprocess(style_image_path)
+	model = StyleTransferModel(content_image, style_image)
 	image = None
 	for step in range(hp.num_step * hp.num_epoch):
-		loss, image = train_step(run.content_image)
+		loss, image = train_step(model)
 		tf.print(loss)
-	model.trainable = False
+
 	# Output the image
 	image = run.postprocess(image)
 	image.save("./trivial_result/new_result.jpg")
-	test_image = run.preprocess("./trivial_data/architecture.jpg")
-	_, image = model(test_image)
-	image = run.postprocess(image)
-	image.save("./trivial_result/test_result.jpg")
+	# test_image = preprocess("./trivial_data/architecture.jpg")
+	# _, image = model(test_image)
+	# image = postprocess(image)
+	# image.save("./trivial_result/test_result.jpg")
 
 if __name__ == "__main__":
 	main()
