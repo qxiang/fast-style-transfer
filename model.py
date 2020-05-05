1746b7b84328f4d5e34297e682116fc30884cc41
Qian Xiang <qxiang@Qians-MacBook-Pro.local>
2020-05-05 15:41:26 -0400

    Change model nromalization layer

--- model.py
+++ model.py
@@ -43,13 +43,13 @@ class TransNet(nn.Module):
         self.model = nn.Sequential(
             # Downsampling subnet.
             nn.Conv2d(3, 32, 9, 1, 4, padding_mode='reflect'),
-            nn.BatchNorm2d(32),
+            nn.InstanceNorm2d(32),
             nn.ReLU(),
             nn.Conv2d(32, 64, 3, 2, 1, padding_mode='reflect'),
-            nn.BatchNorm2d(64),
+            nn.InstanceNorm2d(64),
             nn.ReLU(),
             nn.Conv2d(64, 128, 3, 2, 1, padding_mode='reflect'),
-            nn.BatchNorm2d(128),
+            nn.InstanceNorm2d(128),
             nn.ReLU(),
 
             # Residual blocks.
@@ -61,10 +61,10 @@ class TransNet(nn.Module):
 
             # Upsampling subnet.
             nn.ConvTranspose2d(128, 64, 3, 2, 1, 1),
-            nn.BatchNorm2d(64),
+            nn.InstanceNorm2d(64),
             nn.ReLU(),
             nn.ConvTranspose2d(64, 32, 3, 2, 1, 1),
-            nn.BatchNorm2d(32),
+            nn.InstanceNorm2d(32),
             nn.ReLU(),
             nn.Conv2d(32, 3, 9, 1, 4, padding_mode='reflect'),
             nn.Tanh()
