199c97b32fe76025df827532196a0dc7673b0406
qxiang <qian_xiang@cv.c.cs1430-proj4-270900.internal>
2020-05-06 16:42:35 +0000

    Fix style error

--- train.py
+++ train.py
@@ -17,7 +17,7 @@ def _content_loss(input, target):
 def _gram(input):
     a, b, c, d = input.size()
     input = input.view(a, b, c * d)
-    return torch.bmm(input, input.transpose(1, 2)).div(a * b * c * d)
+    return torch.bmm(input, input.transpose(1, 2)).div(b * c * d)
 
 def _style_loss(input, target):
     return F.mse_loss(_gram(input), _gram(target).detach())
